f23eae8800d9283b24b6a007e0d6d2bc8a48eb47
nazibanuma <nazibanuma@gmail.com>
2025-10-26 23:29:47 +0600

    26/10

--- GIt/src/git/GIt.java
+++ GIt/src/git/GIt.java
@@ -1,9 +1,11 @@
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
 import java.util.regex.*;
 
 class GitHub {
+
     public static void main(String[] args) throws Exception {
         if (args.length == 0) {
             System.out.println("Usage: java GitHubStreaks <username>");
@@ -18,7 +20,9 @@ class GitHub {
         Matcher m = p.matcher(html);
 
         TreeMap<String, Integer> map = new TreeMap<>();
-        while (m.find()) map.put(m.group(1), Integer.parseInt(m.group(2)));
+        while (m.find()) {
+            map.put(m.group(1), Integer.parseInt(m.group(2)));
+        }
 
         int longest = 0, current = 0, streak = 0;
         List<String> dates = new ArrayList<>(map.keySet());
@@ -26,11 +30,15 @@ class GitHub {
             if (map.get(d) > 0) {
                 streak++;
                 longest = Math.max(longest, streak);
-            } else streak = 0;
+            } else {
+                streak = 0;
+            }
         }
 
         // Count current streak backwards
-        for (int i = dates.size() - 1; i >= 0 && map.get(dates.get(i)) > 0; i--) current++;
+        for (int i = dates.size() - 1; i >= 0 && map.get(dates.get(i)) > 0; i--) {
+            current++;
+        }
 
         System.out.println("Current streak: " + current + " day(s)");
         System.out.println("Longest streak: " + longest + " day(s)");
@@ -40,9 +48,10 @@ class GitHub {
         URL url = new URL(urlStr);
         BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
         StringBuilder sb = new StringBuilder();
-        for (String line; (line = in.readLine()) != null;) sb.append(line);
+        for (String line; (line = in.readLine()) != null;) {
+            sb.append(line);
+        }
         in.close();
         return sb.toString();
     }
 }
- 
